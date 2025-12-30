909adf491a45ec414d06435912981f9648a45dd4
Flower <gabrielmoita34@gmail.com>
2025-12-30 11:37:03 +0000

    Did RF33 - compiled without error

---
+++ teste.java
@@ -0,0 +1,13 @@
+package trabalhoprog;
+
+//Flower: started from RF33
+public class teste {
+	String designacao;
+	float valor_medido;
+	float valor_referencia;
+	String unidade_medida;
+
+	public teste(){
+	
+	}
+}
