a51d0a9dfef52a59e3136aca1a190bc90eaca155
Flower <gabrielmoita34@gmail.com>
2026-01-17 11:35:26 -0500

    Did RF9, created a quick temp fix for the paths thingy, will finish later, added card to trello

--- FileHandler.java
+++ FileHandler.java
@@ -11,21 +11,22 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class FileHandler {
-    String user;
-    String password;
-//Flower: see if this is really necessary to be here, need to test on other devices after
-//pathes suck
+    private String user;
+    private String password;
+    //Flower: see if this is really necessary to be here, need to test on other devices after
+    //paths suck
 //=========================================================
-    private static Path s = Paths.get("");
+    private static Path s = Paths.get("").toAbsolutePath();
     private static String user_path = s.toString() ;
-
-    //FLower: how the hell is this diferent from like 6 lines bellow????????
+    
+    //Flower: how the hell is this diferent from like 6 lines bellow????????
     //private String path_users = "trabalhoprog/Documents/users.txt";
 
 //========================================================
     private static Vector<String> return_user(){
         //YK error prone stuff
         //ass: Flower
+	//System.out.println("!DEBUG!: " +  path_user);
         File myObj = new File("trabalhoprog/Documents/users.txt");
         Vector<String> users = new Vector<>();
 
@@ -53,61 +54,64 @@ public class FileHandler {
             String filePassword = data[1];
             
             if(fileUsername.equals(username) && filePassword.equals(password)) {
-                return true;
+              //Users user = new Users(username, password, username, true, "cliente");  
+              return true;
             }
         }
 
         return false;
     }
 /* Flower
-    TODO: can turn this two functions into one
-	TODO: fix the file location finding thingy	
+ * %TEMP FIX%
+ *  TODO: can turn this two functions into one
+ *  will probably do this more to the end of the work
+ *	TODO: fix the file location finding thingy	
 ===============================================================================================*/
 
     public static void saveUser(Users user){
         try(FileWriter writer = new FileWriter("trabalhoprog/Documents/users.txt", true)){
             writer.write(user.toFileString() + "\n");
         } catch(IOException e){
-		System.out.println("Working directory: " + System.getProperty("user.dir"));
+		        System.out.println("Working directory: " + System.getProperty("user.dir"));
             System.out.println("!FileHandler!: Error saving user: " + e.getMessage());
         }
     }
 
     public static void saveAdm(Users user){
-        //try(FileWriter writer = new FileWriter(user_path + "/Documents/admins.txt", true)){
-	try(FileWriter writer = new FileWriter("trabalhoprog/Documents/admins.txt", true)){
-    		    System.out.println(user.toFileString());
+      //try(FileWriter writer = new FileWriter(user_path + "/Documents/admins.txt", true)){
+	    try(FileWriter writer = new FileWriter("trabalhoprog/Documents/admins.txt", true)){
+    		System.out.println(user.toFileString());
 
-		writer.write(user.toFileString() + "\n"); 	
-	} catch(IOException e){
-            System.out.println("!FileHandler!: Error saving admin: " + e.getMessage());
-        }
+		    writer.write(user.toFileString() + "\n"); 	
+	    } catch(IOException e){
+        System.out.println("!FileHandler!: Error saving admin: " + e.getMessage());
+      }
     }
     
 //=============================================================================================
 
     public static boolean checkadm(){
-        /*Flower
-            TODO: pretty much everything of this stuff was taken from the read user func
-            maybe we can do smth about it
-Todo: fix the file location finder thing here
-        */
-        File myObj = new File("trabalhoprog/Documents/admins.txt");
-        Vector<String> admin = new Vector<>();
+      /*Flower
+          TODO: pretty much everything of this stuff was taken from the read user func
+          maybe we can do smth about it
+      Todo: fix the file location finder thing here
+      */
+      File myObj = new File("trabalhoprog/Documents/admins.txt");
+      Vector<String> admin = new Vector<>();
         
-        try(Scanner myReader = new Scanner(myObj)){
-            while(myReader.hasNextLine()){
-                String data = myReader.nextLine();
-                admin.add(data);
-            }
-        }catch (FileNotFoundException flf){
-            System.out.println("!FileHandler!: Error reading file adm: " + flf.getMessage());
+      try(Scanner myReader = new Scanner(myObj)){
+        while(myReader.hasNextLine()){
+          String data = myReader.nextLine();
+          admin.add(data);
         }
+      }catch (FileNotFoundException flf){
+        System.out.println("!FileHandler!: Error reading file adm: " + flf.getMessage());
+      }
 
-        if(admin.isEmpty()){
-            return true; //the vector is empty
-        } else {
-            return false;
-        }
+      if(admin.isEmpty()){
+        return true; //the vector is empty
+      } else {
+        return false;
+      }
     }
 }
--- Main.java
+++ Main.java
@@ -2,81 +2,92 @@ package trabalhoprog;  //Flower this is the name of the directory where I'm runn
 import java.util.Scanner;
 
 public class Main {
-	
-    public static void main(String[] args){
-        //Flower: debugging things
-	System.out.println("Working directory: " + System.getProperty("user.dir"));
+	private static Users user = new Users("no_ussername", "no_password", "no_name", true, "not_defined"); 
+
+  public static void main(String[] args){
+    //Flower: debugging things
+	  System.out.println("Working directory: " + System.getProperty("user.dir"));
 
 	    
-	if(!FileHandler.checkadm()){
-            Scanner input = new Scanner(System.in);
+	  if(!FileHandler.checkadm()){
+      Scanner input = new Scanner(System.in);
 
-            System.out.println("Please choose what you want to do: " + "\n" + "1- loggin" + "\n" + "2- Sign in");
-            int choice = input.nextInt();
+      System.out.println("Please choose what you want to do: " + "\n" + "1- loggin" + "\n" + "2- Sign in");
+      int choice = input.nextInt();
 
-            if(choice == 1){
-                login();
-            } else if(choice == 2){
-		Sign_up();
-            }
+      if(choice == 1){
+        login();
+      } else if(choice == 2){
+		    Sign_up();
+      }
 
-            input.close();
-        } else {
-            System.out.println("No admin found please create on adm");
-            //Flower
-            //TODO: create a create adm function
-		Admin.create_admin();
-        }
+      input.close();
+
+      on_exit();
+    } else {
+      System.out.println("No admin found please create on adm");
+      //Flower
+      //TODO: create a create adm function
+		  Admin.create_admin();
     }
+  }
 
-    private static void login(){
-       Scanner input = new Scanner(System.in);
-        //FileHandler fileHandler = new FileHandler();
+  private static void on_exit(){
+    //find a way to get username
+    System.out.println("Adeus " + user.return_user());
+  }
 
-        /*TODO: Flower: this is only temporary, and only aplies to users, like, you cant
+  private static void login(){
+    Scanner input = new Scanner(System.in);
+    //FileHandler fileHandler = new FileHandler();
+
+    /*TODO: Flower: this is only temporary, and only aplies to users, like, you cant
             choose if you want to be logged in as admin or user or etc...
-        */
+    */
 
-	//Flower
-	//TODO: Precisamos de traduzir todos os System.out.println que temos bro
-        System.out.println("Please insert your username: ");
-        String username = input.nextLine();
+	  //Flower
+	  //TODO: Precisamos de traduzir todos os System.out.println que temos bro
+    System.out.println("Please insert your username: ");
+    String username = input.nextLine();
 
-        System.out.println("Please insert your password: ");
-        String password = input.nextLine();
+    System.out.println("Please insert your password: ");
+    String password = input.nextLine();
 
-        if(FileHandler.login(username, password)){
-           // System.out.println("The loggin was succesfull");
-	   //Flower:try to find out why not working later
-	   //System.out.println("Bem-vindo" + Users.return_user);
-        	System.out.println("Bem vindo " + username);
-	} else {
-            System.out.println("The loggin was not succesfull");
-        }
-
-        input.close();
+    if(FileHandler.login(username, password)){
+      // System.out.println("The loggin was succesfull");
+	    //Flower:try to find out why not working later
+	    //System.out.println("Bem-vindo" + Users.return_user);
+      System.out.println("Bem vindo " + username);
+      user.setUsermame(username);
+      user.setPassword(password);
+	  } else {
+      System.out.println("The loggin was not succesfull");
     }
 
-    private static void Sign_up(){
-        /*FLower
-            TODO: pass this to user class
-        */
-        Scanner input = new Scanner(System.in);
-        //FileHandler fileHandler = new FileHandler();
-        Users user = new Users("no_ussername", "no_password", "no_name", false, "user");
+    input.close();
+  }
 
-        /*Flower
-            TODO: theres this little errors but it works + part of this code is repeated from the loggin, maybe we can do smth about this
-         */
+  private static void Sign_up(){
+    /*FLower
+      TODO: pass this to user class
+    */
+    Scanner input = new Scanner(System.in);
+    //FileHandler fileHandler = new FileHandler();
+    user.setType("user");
 
-        System.out.println("Please insert your username: ");
-        String username = input.nextLine();
+    /*Flower
+     TODO: theres this little errors but it works + part of this code is repeated from the loggin, maybe we can do smth about this
+    */
 
-        System.out.println("Please insert your password: ");
-        String password = input.nextLine();
+    System.out.println("Please insert your username: ");
+    String username = input.nextLine();
 
-        user.setUsermame(username);
-        user.setPassword(password);
-        FileHandler.saveUser(user);
-    }
+    System.out.println("Please insert your password: ");
+    String password = input.nextLine();
+
+    user.setUsermame(username);
+    user.setPassword(password);
+    FileHandler.saveUser(user);
+  }
+
 }
