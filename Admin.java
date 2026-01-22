a1a01c1ff3b8bbdef22679c5f24a394496a37cb6
Flower <gabrielmoita34@gmail.com>
2026-01-21 23:47:15 -0500

    finished last bug run tests, R11 and R18 are implemented without erros

--- Admin.java
+++ Admin.java
@@ -48,23 +48,28 @@ public class Admin extends Users{
 
   public void seeSignUpRequests(dados data, Scanner input){
     Vector<Users> requests = data.return_user_requests();
-    //Flower
-    //Here we print like... all of the User's infor or....?
-    //like for simplicity's sake I'm just fonna print the name and like type ig but we deff gotta decide this
-    Users user;
-    for(int i = 0; i < requests.size(); i++){
-      user = requests.get(i);
-      System.out.println("pedido " + i + " - username: " + user.return_user() + " | tipo: " + user.return_type() + "\n");
+    
+    if(requests.size() == 0){
+      System.out.println("Nao ha pedidos de Registo neste momento");
+    } else {
+      //Flower
+      //Here we print like... all of the User's infor or....?
+      //like for simplicity's sake I'm just fonna print the name and like type ig but we deff gotta decide this
+      Users user;
+      for(int i = 0; i < requests.size(); i++){
+        user = requests.get(i);
+        System.out.println("pedido " + i + " - username: " + user.return_user() + " | tipo: " + user.return_type() + "\n");
 
+      }
+      System.out.println("Por favor escolha um pedido para aceitar");
+      int choice = input.nextInt();
+      input.nextLine();
+
+      user = requests.get(choice);
+      FileHandler.saveCredentials(user.return_user(), user.return_password());
+      data.remove_request(choice);
+      data.add_User(user);
     }
-    System.out.println("Por favor escolha um pedido para aceitar");
-    int choice = input.nextInt();
-    input.nextLine();
-
-    user = requests.get(choice);
-    FileHandler.saveCredentials(user.return_user(), user.return_password());
-    data.remove_request(choice);
-    data.add_User(user);
   }
 
   public void accept_requests(dados data, Scanner input){
@@ -72,76 +77,69 @@ public class Admin extends Users{
     //this is basically just like copied from Technical.java, because like R13 says that tecnicos must accept the service,
     //but then R18 and R20 says that the Admin must accept, idk bro I just did both out of confusion
     Vector<Services> services = data.return_service_request();
-
-    System.out.println("Escolha qual servico selecionar");
-
-    Services service;
-    for(int i = 0; i < services.size(); i++){
-      service = services.get(i);
-      System.out.println("service " + i + ":" + " codigo:" + service.get_code());
-    }
-    int choice = input.nextInt();
-    input.nextLine();
     
-    service = services.get(choice);
-    System.out.println();
-    System.out.println("Escolha o que fazer: 1- Aprovar 2- Rejeitar");
-    int decition = input.nextInt();
-    input.nextLine();
-
-    if(decition == 1){
-      service.setState("aprovado");
-      associateTecnitians(data, input, service);
+    if(services.size() == 0){
+      System.out.println("Nao ha nenhum servico sem tecnico responsavel neste momento");
     } else {
-      service.setState("rejeitado");
-    }
+      System.out.println("Escolha qual servico selecionar");
 
-    data.remove_service_request(choice);
-    data.add_service(service);
+      Services service;
+      for(int i = 0; i < services.size(); i++){
+        service = services.get(i);
+        System.out.println("service " + i + ":" + " codigo:" + service.get_code());
+      }
+      int choice = input.nextInt();
+      input.nextLine();
+    
+      service = services.get(choice);
+      System.out.println();
+      System.out.println("Escolha o que fazer: 1- Aprovar 2- Rejeitar");
+      int decition = input.nextInt();
+      input.nextLine();
+    
+      int result;
+      if(decition == 1){
+        service.setState("aprovado");
+        result = associateTecnitians(data, input, service);
+      } else {
+        service.setState("rejeitado");
+        result = 1; 
+      }
+    
+      if(result == 1){ //tecnico finished without issue 
+        data.remove_service_request(choice);
+        data.add_service(service);
+      } else {
+        System.out.println("Desculpe nao conseguimos processar o servico, progresso nao salvo");
+      }
+    }
   }
   
   //Flower
   //R11 and R18 are like the same?? continuation???? 
   //like I'm just gonna make it a continuation.....
-  public void associateTecnitians(dados data, Scanner input, Services service){
+  public int associateTecnitians(dados data, Scanner input, Services service){
     Vector<Technical> tecnicos = data.return_tecnicos();
-
-    Technical tecnico;
-    System.out.println("escolha um tecnico para associar ao servico \n");
-    for(int i = 0; i < tecnicos.size(); i++){
-      tecnico = tecnicos.get(i);
-      
-      //TODO: add more info to the print if necessary
-      System.out.println("tecnico " + i + "- nome: " + tecnico.return_user() + "\n");
-    }
-    int choice = input.nextInt();
-    input.nextLine();
-    tecnico = tecnicos.get(choice);
     
-    /*
-    Vector<Services> services = data.return_services();
-    //Flower: 
-    //like ok same question as in the method before like... what exactly do we print?
-    //TODO: RODRIGO VE AS MENSAGENS QUE TE MANDEI A PERGUNTAR SOBRE A INTERPRETACAO DISTO, obrigado :)
-    Services service;
-    System.out.println("Escolha um servico ao tecnico " + tecnico.return_user() + " ser associado \n");
-    for(int i = 0; i < services.size(); i++){
-      service = services.get(i);
+    if(tecnicos.size() == 0){
+      System.out.println("Nao ha nenhum tecnico disponivel");
+
+      return 0;
+    } else {
+      Technical tecnico;
+      System.out.println("escolha um tecnico para associar ao servico \n");
+      for(int i = 0; i < tecnicos.size(); i++){
+        tecnico = tecnicos.get(i);
       
-      //Flower: for simplicity sake I'll only print the tecnico's name
-      tecnico = service.return_tecnico();
-      //and like, I'm not sure if I should just print every attribute of service or not so.... yeah... just tale the code and tecnico
-      System.out.println("servico n-" + i + ": codigo " + service.get_code() + " | tecnico responsavel: " + tecnico.return_user());
+        //TODO: add more info to the print if necessary
+        System.out.println("tecnico " + i + "- nome: " + tecnico.return_user() + "\n");
+      }
+      int choice = input.nextInt();
+      input.nextLine();
+      tecnico = tecnicos.get(choice);
+      service.set_Tecnico(tecnico);
+
+      return 1;
     }
-    choice = input.nextInt();
-    input.nextLine();
-    data.remove_service(choice);
-
-    service = services.get(choice);
-
-    service.set_Tecnico(tecnico);
-    data.add_service(service);
-    */
-    service.set_Tecnico(tecnico);
   } 
 }
